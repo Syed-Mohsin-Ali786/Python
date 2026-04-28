0b7c2eebd51db4c3441f45a21052511dd643a59b
Syed-Mohsin-Ali786 <syedmohsinali4345@gmail.com>
2026-04-28 10:55:17 +0500

    28 April 2026

---
+++ main.py
@@ -0,0 +1,25 @@
+# a=3
+# b=4
+# print(a**b) 
+# print(a*"ali")
+
+# print("hello world",end=" \n"*3)
+# print("welcome to python programming")
+# print(f"python is a great {"\t"*3}  language")
+# print(False==0)
+
+# account="12\n3"
+
+# print(account*3)
+
+# value=3.45
+# value=round(value)
+# print(value)
+
+# a="ali",
+# b=a="petter"
+# print(a)
+
+# imaginary=1+3j
+# print (imaginary+3+2j)
+
